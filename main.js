d6189a5b1227f30aab28dccfbdf7446f8fb9ab6b
jaimezzapata <zapataval2304@GMAIL.COM>
2023-01-30 11:07:09 -0500

    Se crea proyecto de cajero por consola

---
+++ main.js
@@ -0,0 +1,84 @@
+/* let nombres = []
+
+for (let index = 0; index < 5; index++) {
+  nombres[index] = prompt('Ingrese un nombre')
+}
+
+for (let index = 0; nombres.length; index++) {
+  console.log(nombres[index])
+}
+ */
+
+/* Metodos Arreglos JavaScript */
+
+/* 
+let numeros = [1,2,3,4]
+
+for (let index = 0; index < numeros.length; index++) {
+  console.log(numeros[index])
+}
+
+numeros.push(5)
+numeros[3] = 5
+
+for (let index = 0; index < numeros.length; index++) {
+  console.log(numeros[index])
+}
+
+for (let index = 0; index < 10; index++) {
+  numeros[3] = 5
+  numeros[4] = 5
+  numeros[5] = 5
+  numeros[6] = 5
+  numeros[7] = 5
+  numeros[8] = 5
+  numeros[9] = 5
+  numeros[10] = 5
+}
+
+for (let index = 0; index < 10; index++) {
+  numeros.push() = prompt('Ingrese un numero: ')
+} */
+
+let saldoCuenta = 0;
+let valor = 0;
+let repetir = true
+
+while (repetir) {
+  let menu = prompt(
+    "Seleccione la operacón que desea realizar \n1 - Consultar saldo \n2 - Retirar Dinero \n3 - Consignar dinero \n4 - Transferir \n5 - Salir"
+  );
+  switch (parseInt(menu)) {
+    case 1:
+      alert("Su saldo es: " + saldoCuenta);
+      break;
+    case 2:
+      valor = Number(prompt("Inrgese el valor a retirar: "));
+      if (valor > saldoCuenta) {
+        alert("El saldo en su cuenta es menor al valor a retirar");
+      } else {
+        alert(
+          "Retiro Exitoso. Su nuevo saldo es: " +
+            (saldoCuenta = saldoCuenta - valor)
+        );
+        alert("Retiro Exitoso. Su nuevo saldo es: " + (saldoCuenta -= valor));
+      }
+      break;
+    case 3:
+      valor = Number(prompt("Inrgese el valor a retirar: "));
+      saldoCuenta += valor;
+      alert("El nuevo saldo de la cuenta es: " + saldoCuenta);
+      break;
+    case 4:
+      valor = Number(prompt("Inrgese el valor a retirar: "));
+      if (valor > saldoCuenta) {
+        alert("El saldo en su cuenta es menor al valor a transferir");
+      } else {
+        alert("Retiro Exitoso. Su nuevo saldo es: " + (saldoCuenta -= valor));
+      }
+      break;
+    case 5:
+      repetir = false
+      break;
+  }
+}
